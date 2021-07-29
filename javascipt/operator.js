64a3885f7846ab8a7fc90e4279631051ebeb82ed
Kisensei <210008@softwarica.edu.np>
2021-07-29 13:59:47 +0545

    started

---
+++ javascipt/operator.js
@@ -0,0 +1,26 @@
+// function myfunction() {
+// console.log("woithout para");
+    
+// }
+// myfunction();
+// function sum(a,b) {
+//     var c=a+b;
+//     console.log(c)
+// }
+// sum(5,7);
+// sum(8,9);
+
+// const sum2=(a,b)=>{
+    
+// }
+
+var mystring ="softwarica o e commerce";
+console.log(mystring.length);
+let mynumber = "20";
+var mynumber1 ="20";
+console.log(mynumber1.toString() + mynumber.toString());
+var x="10.555";
+var y=parseInt(x);
+console.log(y);
+var z=parseFloat(x);
+console.log(z);
---
+++ javascipt/script.js
@@ -0,0 +1,34 @@
+// function myfunction()
+// {
+//     alert("button clicked")
+//     console.log("this is connsole for javascript")
+//     window.AbortSignal("abort")
+// }
+// // document.getElementById("para1").innerHTML= "this is text from java script"
+// var x="sushil",
+// y=4.
+// z=6;
+// var myArray = ["sus","sush","sushi","sushil"]
+// console.log(myArray[1]);
+// var babale;
+// console.log(babale);
+
+// var bablu="ray";
+// function bablufunction()
+// {
+// console.log(bablu)    ;
+// }
+// var i=9;
+// for(var i=0; i<1; i++)
+
+// console.log(i);
+
+// let j=0;
+// for (let j=0 ; j<10; j++)
+// {console.log(j);}
+// console.log(j)
+// var x="5";0
+// var y="4";
+
+// var z= x*y;
+// console.log(z);
